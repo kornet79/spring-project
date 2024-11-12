d951ceb85eb8d9cd827fdfc8fb6f53d695e89079
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-11-13 08:08:16 +0900

    Read only 부분 고침

--- src/main/java/com/example/fistproject2/dto/ArticleFrom.java
+++ src/main/java/com/example/fistproject2/dto/ArticleFrom.java
@@ -1,24 +1,16 @@
 package com.example.fistproject2.dto;
 
 import com.example.fistproject2.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+@ToString
+@AllArgsConstructor
 
 public class ArticleFrom {
 
     private String title;
     private String content;
 
-    public ArticleFrom(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleFrom{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     // ntt 를 변환 해주는 메소드가 되는거임
     public Article toEntity() {
